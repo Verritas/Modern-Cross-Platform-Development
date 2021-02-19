463d91b543c0cd6ad32236acc9029ceb989a8a23
Verritas <example@example.com>
2021-02-19 16:54:37 +0300

    Chapter05
    Chapter05

---
+++ Chapter05/PacktLibrary9/FlightPatterns.cs
@@ -0,0 +1,27 @@
+﻿namespace Packt.Shared
+{
+    public class BusinessClassPassanger
+    {
+        public override string ToString()
+        {
+            return $"Business Class";
+        }
+    }
+
+        public class FirstClassPassanger {
+            public int AirMiles {get;set;}
+            public override string ToString()
+            {
+                return $"First Class with {AirMiles:N0} air miles";
+            }
+        }
+
+        public class CoachClassPassanger {
+            public double CarryOnKG {get;set;}
+            public override string ToString()
+            {
+                return $"Coach Class with {CarryOnKG:N2} KG carry on";
+            }
+
+        }
+}
