60a59631a465e1d008c2d03e0b419f1abcee3d46
Verritas <example@example.com>
2021-03-20 13:25:50 +0300

    Chapter08
    Chapter08

---
+++ Chapter08/Exersice02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exersice02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do {
+            Console.WriteLine("Please print a regular expression: ");
+            string regexString = Console.ReadLine();
+
+            Console.WriteLine("Please print some input: ");
+            string input = Console.ReadLine();
+            Regex regex = new Regex(regexString);
+            
+            Console.WriteLine("{0} mathes {1}? {2}", arg0:input, arg1:regexString, arg2:regex.IsMatch(input).ToString());
+            Console.WriteLine("Press ESC to end or any key to try again");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ Chapter08/Exersice03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Exersice03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<UInt64, string> nums = new Dictionary<UInt64, string>();
+            nums.Add(1000000000000000000, "quintillion");
+            nums.Add(1000000000000000, "quadrillion");
+            nums.Add(1000000000000, "trillion");
+            nums.Add(1000000000, "billion");
+            nums.Add(1000000, "million");
+            nums.Add(1000, "thousand");
+            nums.Add(100, "hundred");
+            nums.Add(10, "ten");
+            nums.Add(1, "one");
+            
+            var num = (UInt64)1211241234;
+            foreach (var item in nums)
+            {
+                UInt64 div = num / Convert.ToUInt64(item.Key);
+                if (div!=0) {
+                    Console.Write($"{div} {getS(div, item.Value)} ");
+                }
+                num = num - div*Convert.ToUInt64(item.Key);
+            }
+        }
+
+        private static string getS(BigInteger num, string str) {
+            if ((num==1)||(num==0)) return str;
+            else return str+"s";
+        }
+    }
+}
