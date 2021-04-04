7470b1ae8dff4304b6c7d5afe5eb13d3c847e97e
Verritas <example@example.com>
2021-04-04 11:10:18 +0300

    Chapter11
    Chapter11

---
+++ Chapter11/Exersice02/Program.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+using static System.Console;
+using static System.IO.Path;
+using static System.Environment;
+using System.IO;
+using System.Xml;
+
+using System.Text.Json;
+using Exercise02;
+
+namespace Exercise02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WriteLine("Creating four files containing serialized categories and products...");
+
+      using (var db = new Northwind())
+      {
+        IQueryable<Category> cats = db.Categories.Include(c => c.Products);
+
+        GenerateXmlFile(cats);
+        GenerateXmlFile(cats, useAttributes: false);
+        GenerateCsvFile(cats);
+        GenerateJsonFile(cats);
+      }
+    }
+
+    private delegate void WriteDataDelegate(string name, string value);
+
+    private static void GenerateXmlFile(IQueryable<Category> cats, bool useAttributes = true)
+    {
+      string which = useAttributes ? "attibutes" : "elements";
+
+      string xmlFile = $"categories-and-products-using-{which}.xml";
+
+      using (FileStream xmlStream = File.Create(
+        Combine(CurrentDirectory, xmlFile)))
+      {
+        using (XmlWriter xml = XmlWriter.Create(xmlStream,
+          new XmlWriterSettings { Indent = true }))
+        {
+
+          WriteDataDelegate writeMethod;
+
+          if (useAttributes)
+          {
+            writeMethod = xml.WriteAttributeString;
+          }
+          else
+          {
+            writeMethod = xml.WriteElementString;
+          }
+
+          xml.WriteStartDocument();
+          xml.WriteStartElement("categories");
+
+          foreach (Category c in cats)
+          {
+            xml.WriteStartElement("category");
+            writeMethod("id", c.CategoryID.ToString());
+            writeMethod("name", c.CategoryName);
+            writeMethod("desc", c.Description);
+            writeMethod("product_count", c.Products.Count.ToString());
+            xml.WriteStartElement("products");
+
+            foreach (Product p in c.Products)
+            {
+              xml.WriteStartElement("product");
+
+              writeMethod("id", p.ProductID.ToString());
+              writeMethod("name", p.ProductName);
+              writeMethod("cost", p.Cost.Value.ToString());
+              writeMethod("stock", p.Stock.ToString());
+              writeMethod("discontinued", p.Discontinued.ToString());
+
+              xml.WriteEndElement();
+            }
+            xml.WriteEndElement();
+            xml.WriteEndElement();
+          }
+          xml.WriteEndElement();
+        }
+      }
+
+      WriteLine("{0} contains {1:N0} bytes.",
+        arg0: xmlFile,
+        arg1: new FileInfo(xmlFile).Length);
+    }
+
+    private static void GenerateCsvFile(IQueryable<Category> cats)
+    {
+      string csvFile = "categories-and-products.csv";
+
+      using (FileStream csvStream = File.Create(Combine(CurrentDirectory, csvFile)))
+      {
+        using (var csv = new StreamWriter(csvStream))
+        {
+
+          csv.WriteLine("CategoryID,CategoryName,Description,ProductID,ProductName,Cost,Stock,Discontinued");
+
+          foreach (Category c in cats)
+          {
+            foreach (Product p in c.Products)
+            {
+              csv.Write("{0},\"{1}\",\"{2}\",",
+                arg0: c.CategoryID.ToString(),
+                arg1: c.CategoryName,
+                arg2: c.Description);
+
+              csv.Write("{0},\"{1}\",{2},",
+                arg0: p.ProductID.ToString(),
+                arg1: p.ProductName,
+                arg2: p.Cost.Value.ToString());
+
+              csv.WriteLine("{0},{1}",
+                arg0: p.Stock.ToString(),
+                arg1: p.Discontinued.ToString());
+            }
+          }
+        }
+      }
+
+      WriteLine("{0} contains {1:N0} bytes.",
+        arg0: csvFile,
+        arg1: new FileInfo(csvFile).Length);
+    }
+
+    private static void GenerateJsonFile(IQueryable<Category> cats)
+    {
+      string jsonFile = "categories-and-products.json";
+
+      using (FileStream jsonStream = File.Create(Combine(CurrentDirectory, jsonFile)))
+      {
+        using (var json = new Utf8JsonWriter(jsonStream,
+          new JsonWriterOptions { Indented = true }))
+        {
+          json.WriteStartObject();
+          json.WriteStartArray("categories");
+
+          foreach (Category c in cats)
+          {
+            json.WriteStartObject();
+
+            json.WriteNumber("id", c.CategoryID);
+            json.WriteString("name", c.CategoryName);
+            json.WriteString("desc", c.Description);
+            json.WriteNumber("product_count", c.Products.Count);
+
+            json.WriteStartArray("products");
+
+            foreach (Product p in c.Products)
+            {
+              json.WriteStartObject();
+
+              json.WriteNumber("id", p.ProductID);
+              json.WriteString("name", p.ProductName);
+              json.WriteNumber("cost", p.Cost.Value);
+              json.WriteNumber("stock", p.Stock.Value);
+              json.WriteBoolean("discontinued", p.Discontinued);
+
+              json.WriteEndObject();
+            }
+            json.WriteEndArray();
+            json.WriteEndObject();
+          }
+          json.WriteEndArray();
+          json.WriteEndObject();
+        }
+      }
+
+      WriteLine("{0} contains {1:N0} bytes.",
+        arg0: jsonFile,
+        arg1: new FileInfo(jsonFile).Length);
+    }
+  }
+}
---
+++ Chapter11/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Proxies;
+
+namespace Packt.Shared
+{
+    public class Northwind : DbContext
+    {
+        public DbSet<Category> Categories {get;set;}
+        public DbSet<Product> Products {get;set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
+            optionsBuilder.UseLazyLoadingProxies().UseSqlite($"Filename={path}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Category>().Property(category => category.CategoryName).IsRequired().HasMaxLength(15);
+            modelBuilder.Entity<Product>().Property(product => product.Cost).HasConversion<double>();
+
+            modelBuilder.Entity<Product>().HasQueryFilter(p=>!p.Discontinued);
+        }
+    }
+}
