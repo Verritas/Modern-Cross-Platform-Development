41eb900b5d0b2ceb3dbfef5f2f7c8d645ba18b0b
Verritas <example@example.com>
2021-04-26 22:27:03 +0300

    Chapter13
    Chapter13

---
+++ Chapter13/SyncronizingResourceAccess/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+namespace SyncronizingResourceAccess
+{
+    class Program
+    {
+        static Random r = new Random();
+        static string Message;
+        static object conch = new object();
+
+        static void Main(string[] args)
+        {
+            WriteLine("Please wait for the tasks to complete.");
+            Stopwatch watch = Stopwatch.StartNew();
+            Task a = Task.Factory.StartNew(MethodA);
+            Task b = Task.Factory.StartNew(MethodB);
+            Task.WaitAll(new Task[] {a, b});
+            WriteLine();
+            WriteLine($"Results: {Message}.");
+            WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+        }
+
+        static void MethodA() {
+
+            try {
+                if (Monitor.TryEnter(conch, TimeSpan.FromSeconds(15))) {
+                    for (int i = 0;i<5;i++) {
+                        Thread.Sleep(r.Next(2000));
+                        Message+="A";
+                        Write(".");
+                    }
+                }
+                else {
+                    WriteLine("Method A failed to enter a monitor lock.");
+                }
+            }
+            finally {
+                Monitor.Exit(conch);
+            }
+        }
+
+        static void MethodB() {
+            lock(conch) {
+            for (int i = 0;i<5;i++) {
+                Thread.Sleep(r.Next(2000));
+                Message+="B";
+                Write(".");
+            }
+            }
+        }
+    }
+}
